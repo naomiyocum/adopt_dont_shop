2af89d1990fe67617bd449896f6186b9de4b0aba
Naomi Yocum <naomiyocum24@gmail.com>
2022-10-24 15:56:06 -0600

    delete notes, whoops sorry

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -11,17 +11,4 @@ class AdminApplicationsController < ApplicationController
       rejected.pet_rejected
     end
   end
-
-  # def update
-  #   @app = Application.find(params[:id])
-  #   if params[:approve]
-  #     approved = ApplicationPet.find_by(application_id: @app.id, pet_id: params[:approve])
-  #     approved = approved.pet_approved
-  #   elsif params[:reject]
-  #     rejected = ApplicationPet.find_by(application_id: @app.id, pet_id: params[:reject])
-  #     rejected = rejected.pet_rejected
-  #   end
-  #   binding.pry
-  #   redirect_to "/admin/applications/#{@app.id}"
-  # end
 end
